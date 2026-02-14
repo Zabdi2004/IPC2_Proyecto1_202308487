9e4ebc9f5eb0375bbcc6099d74825c5457298f3c
Zabdi Donis <3051069530117@ingenieria.usac.edu.gt>
2026-02-14 12:28:37 -0600

    Añadiendo Clases
    He añadido las clases: Paciente, Rejilla, Nodo, ListaEnlazada

---
+++ Proyecto1/Proyecto1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Nodos;
+using ListasEnlazadas;
+using Pacientes;
+
+class Program
+{
+    // Crear una instancia de ListaEnlazada para almacenar pacientes
+    static ListaEnlazada listaPacientes = new ListaEnlazada(); //
+
+    static void Main(string[] args)
+    {
+
+        // Crear algunos pacientes y agregarlos a la lista
+        // Paciente(nombre, edad, periodos, M)
+        Paciente paciente1 = new Paciente("Carlos", 40, 6, 3);
+
+        // Infectar al paciente1 con el virus
+        paciente1.RejillaPaciente.InfectarCelda(1, 1);
+
+        // Agregar los pacientes a la lista enlazada
+        listaPacientes.Agregar(paciente1);
+
+        // Imprimir los pacientes en la lista
+        Console.WriteLine("Lista de Pacientes:");
+        listaPacientes.Recorrer();
+    }
+}
---
+++ Proyecto1/Proyecto1/Rejilla.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rejillas
+{
+    public class Rejilla
+    {
+        public int[,] Matrix;
+        public int Tamaño;
+
+        // Constructor para inicializar la rejilla con un tamaño específico
+        public Rejilla(int m) 
+        { 
+            Tamaño = m;
+            Matrix = new int[m,m];
+        }
+
+        // Método para infectar una celda específica en la rejilla
+        public void InfectarCelda(int fila, int columna)
+        {
+            Matrix[fila, columna] = 1;
+        }
+    }
+}
